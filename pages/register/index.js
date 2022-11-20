47fa04bdd4bf4915272d9a55363b3a08c4f0e796
bkfan1 <jpf8296@gmail.com>
2022-11-20 16:36:05 -0400

    created register page

---
+++ pages/register/index.js
@@ -0,0 +1,10 @@
+import RegisterForm from "../../components/forms/RegisterForm";
+
+
+export default function RegisterPage(){
+    return(
+        <>
+        <RegisterForm/>
+        </>
+    )
+}
