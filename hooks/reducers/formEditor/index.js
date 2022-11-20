2f00a31aba55a4928742d27333f4c1577d607f8b
bkfan1 <jpf8296@gmail.com>
2022-11-19 23:27:58 -0400

    created hook folder + added reducers subfolder

---
+++ hooks/reducers/formEditor/index.js
@@ -0,0 +1,110 @@
+export const reducer = (state, { type, payload }) => {
+    let updatedQuestions = [...state.questions];
+    let index;
+    let found;
+
+    switch (type) {
+      case "UPDATE_FORM_HEADERS": {
+        const { field, newValue } = payload;
+        return { ...state, [field]: newValue };
+      }
+
+      case "ADD_NEW_QUESTION": {
+        const { newQuestion } = payload;
+        updatedQuestions.push(newQuestion);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "DELETE_QUESTION": {
+        const { id } = payload;
+        const updatedQuestions = [...state.questions].filter(
+          (q) => q.id !== id
+        );
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "UPDATE_QUESTION_TITLE": {
+        const { id, newTitle } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+
+        found = updatedQuestions[index];
+
+        found.title = newTitle;
+
+        updatedQuestions.splice(index, 1, found);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "UPDATE_QUESTION_INPUT_DATA": {
+        const { id, newOption, newInputData } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+
+        found = updatedQuestions[index];
+
+        found.option = newOption;
+        found.inputData = newInputData;
+
+        updatedQuestions.splice(index, 1, found);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "UPDATE_QUESTION_REQUIRED": {
+        const { id, newValue } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+
+        found = updatedQuestions[index];
+        found.required = newValue;
+
+        updatedQuestions.splice(index, 1, found);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "MOVE_QUESTION_UP": {
+        const { id } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+        const toMoveUp = updatedQuestions[index];
+
+        updatedQuestions.splice(index, 1);
+        updatedQuestions.splice(index - 1, 0, toMoveUp);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "MOVE_QUESTION_DOWN": {
+        const { id } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+        const toMoveDown = updatedQuestions[index];
+
+        updatedQuestions.splice(index, 1);
+        updatedQuestions.splice(index + 1, 0, toMoveDown);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      case "UPDATE_LINEAR_RANGE_VALUES": {
+        const { id, field, newValue } = payload;
+
+        index = updatedQuestions.findIndex((q) => q.id === id);
+
+        found = updatedQuestions[index];
+        found["inputData"][field] = newValue;
+
+        updatedQuestions.splice(index, 1, found);
+
+        return { ...state, questions: [...updatedQuestions] };
+      }
+
+      default:
+        return state;
+    }
+  };
