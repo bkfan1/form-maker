6216e6bcdef22ae38958fadb4851111748b774c2
bkfan1 <jpf8296@gmail.com>
2022-11-20 16:34:28 -0400

    created folder + moved some components inside of it

---
+++ components/ui/Layout.js
@@ -0,0 +1,15 @@
+
+
+export default function Layout({children}){
+    return(
+        <>
+        <nav>
+
+        </nav>
+        {children}
+        <footer>
+
+        </footer>
+        </>
+    )
+}
---
+++ components/ui/Navbar.js
@@ -0,0 +1,10 @@
+
+export default function Navbar(){
+    return(
+        <>
+        <nav>
+            
+        </nav>
+        </>
+    )
+}
