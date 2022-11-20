34b447be9d12b368fffc05364fc5caf04151db3d
bkfan1 <jpf8296@gmail.com>
2022-11-20 16:35:40 -0400

    created new endpoints

---
+++ pages/api/auth/login/index.js
@@ -0,0 +1,12 @@
+import { handleLogin } from "../../../../middlewares/authentication/login";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      return await handleLogin(req, res);
+
+    default:
+      return await res.status(405).json({message:""});
+      break;
+  }
+}
---
+++ pages/api/register/index.js
@@ -0,0 +1,11 @@
+import { registerAccount } from "../../../middlewares/register";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "POST":
+      return await registerAccount(req, res);
+
+    default:
+      return await res.status(405).json({});
+  }
+}
