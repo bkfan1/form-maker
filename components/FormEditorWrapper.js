504e854ee7c0d1a511373189e171c59fd71183b9
bkfan1 <jpf8296@gmail.com>
2022-11-22 16:59:46 -0400

    Removed unused imports

--- components/FormEditorWrapper.js
+++ components/FormEditorWrapper.js
@@ -2,6 +2,7 @@ import FormEditor from "./FormEditor";
 import FormEditorActionsMenu from "./FormEditorActionsMenu";
 
 export default function FormEditorWrapper() {
+
   return (
     <div className="flex gap-4 items-center">
       <FormEditor />
