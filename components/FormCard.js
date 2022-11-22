69abeb8c5ffd39a1f83668e9947477f5538a5200
bkfan1 <jpf8296@gmail.com>
2022-11-22 16:51:07 -0400

    Added last update info to card

--- components/FormCard.js
+++ components/FormCard.js
@@ -1,21 +1,25 @@
 import Link from "next/link";
 
-export default function FormCard({}) {
-  //const {title, description} = data;
+export default function FormCard({ data }) {
+  const { id, title, updatedAt } = data;
   return (
     <>
-      <figure className="ease-in-out duration-150 w-64 bg-white border rounded shadow-sm hover:shadow-md">
-        <Link href="/">
-          <div className="w-full h-48 bg-gray-500"></div>
+      <figure className="ease-in-out duration-150 w-38 bg-white border rounded shadow-sm hover:shadow-md">
+        <Link href={`/user/forms/edit/${id}`}>
+          <div className="h-48 bg-gray-500"></div>
         </Link>
 
-        <section className="flex flex-col gap-2 p-2">
-          <h1 className="text-gray-800 font-bold">Form title</h1>
+        <section className="flex flex-col gap-1 p-2">
+          <h1 className="text-gray-800 font-bold">{title}</h1>
 
           <div className="flex justify-between items-center">
-            <p className="text-sm text-gray-500">
-              <i className="bi bi-calendar-fill" /> Creation date
-            </p>
+            <div className="flex gap-1 text-sm text-gray-500">
+              <i className="bi bi-file-earmark-text-fill text-xl text-indigo-800" />
+              <div className="flex flex-col">
+                <p className="">Last update:</p>
+                <p>{new Date(updatedAt).toLocaleDateString()}</p>
+              </div>
+            </div>
             <ul className="self-end">
               <li>
                 <button>
