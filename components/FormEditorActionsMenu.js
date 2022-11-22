ce952efde394c02deb6d1efc259a2bff995a7a3d
bkfan1 <jpf8296@gmail.com>
2022-11-22 16:56:17 -0400

    Fixed conditional rendering + added new handler functions

--- components/FormEditorActionsMenu.js
+++ components/FormEditorActionsMenu.js
@@ -1,9 +1,15 @@
 import { nanoid } from "nanoid";
 import { useContext } from "react";
 import { FormEditorContext } from "../contexts/FormEditorContext";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 export default function FormEditorActionsMenu() {
-  const { dispatch } = useContext(FormEditorContext);
+  const router = useRouter();
+  const { pathname } = router;
+
+  const { formData, dispatch, previewForm, setPreviewForm } =
+    useContext(FormEditorContext);
 
   const handleAddNewQuestion = () => {
     const newQuestion = {
@@ -22,31 +28,99 @@ export default function FormEditorActionsMenu() {
     return dispatch({ type: "ADD_NEW_QUESTION", payload: { newQuestion } });
   };
 
+  const handleCreateForm = async () => {
+    const data = { ...formData, updatedAt: new Date() };
+
+    try {
+      const res = await axios.post(`/api/account/forms/`, data);
+      console.log(res.status);
+    } catch (error) {
+      console.warn("error!");
+    }
+  };
+
+  const handleUpdateForm = async () => {
+    const data = { ...formData, updatedAt: new Date() };
+
+    try {
+      const res = await axios.put(`/api/account/forms/${formData.id}`, data);
+      console.log(res.status);
+    } catch (error) {
+      console.warn("error!");
+    }
+  };
+
+  const handleDeleteForm = async () => {
+    try {
+      const res = await axios.delete(`/api/account/forms/${formData.id}`);
+      console.log(res.status);
+      router.push("/user/forms/view");
+    } catch (error) {
+      console.warn("error!");
+    }
+  };
+
   return (
     <>
       <menu className="flex flex-col gap-2 p-2 border rounded bg-white shadow-md">
-        <button
-          onClick={handleAddNewQuestion}
-          title="Add new question"
-          className="text-xl"
-        >
-          <i className="bi bi-plus-circle" />
-        </button>
+        {previewForm && (
+          <button
+            onClick={() => setPreviewForm(!previewForm)}
+            className="text-xl"
+            title="Back to edit mode"
+          >
+            <i className="bi bi-eye" />
+          </button>
+        )}
+        {!previewForm && (
+          <>
+            <button
+              onClick={handleAddNewQuestion}
+              title="Add new question"
+              className="text-xl"
+            >
+              <i className="bi bi-plus-circle" />
+            </button>
 
-        <button
-          title="Save form"
-          className="text-xl"
-        >
-          <i className="bi bi-check-circle" />
-        </button>
+            {pathname === "/user/forms/add" && (
+              <>
+                <button
+                  onClick={handleCreateForm}
+                  title="Create form"
+                  className="text-xl"
+                >
+                  <i className="bi bi-check-circle" />
+                </button>
+              </>
+            )}
 
-        <button className="text-xl">
-          <i className="bi bi-send" title="Publish form" />
-        </button>
+            {formData.questions.length > 0 && (
+              <button
+                onClick={() => setPreviewForm(!previewForm)}
+                className="text-xl"
+                title="Preview form"
+              >
+                <i className="bi bi-eye" />
+              </button>
+            )}
 
-        <button className="text-xl">
-          <i className="bi bi-x-circle" title="Delete form" />
-        </button>
+            {pathname === "/user/forms/edit/[formId]" && (
+              <>
+                <button onClick={handleUpdateForm} className="text-xl">
+                  <i className="bi bi-arrow-up-circle" title="Update form" />
+                </button>
+
+                <button className="text-xl">
+                  <i className="bi bi-send" title="Publish form" />
+                </button>
+
+                <button onClick={handleDeleteForm} className="text-xl">
+                  <i className="bi bi-x-circle" title="Delete form" />
+                </button>
+              </>
+            )}
+          </>
+        )}
       </menu>
     </>
   );
