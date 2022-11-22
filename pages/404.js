8ca78c1f03fea1481ea4fd5af85427fd58fb8a3f
bkfan1 <jpf8296@gmail.com>
2022-11-22 16:48:20 -0400

    Added errors pages

---
+++ pages/404.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function Error404Page(){
+    return(
+        <>
+        <main className="flex flex-col items-center justify-center min-h-screen">
+
+            <figure className="flex flex-col items-center justify-center gap-2 w-64">
+            <i className="bi bi-file-earmark-x-fill text-indigo-800 text-4xl"/>
+                <h1 className="text-xl font-bold">Something went wrong.</h1>
+
+                <Link href="/">
+                <button className="p-2 border rounded text-white bg-indigo-800">Back to app</button>
+                </Link>
+            </figure>
+
+        </main>
+        </>
+    )
+}
