7511812bd63263c5518c207e5880c6fafa5cf0cd
bkfan1 <jpf8296@gmail.com>
2022-12-14 20:47:35 -0400

    Added conditional rendering to improve UI experience

--- pages/submit/[formId].js
+++ pages/submit/[formId].js
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import connection from "../../database/connection";
 import Form from "../../database/models/form";
 
-import ConsumerForm from "../../components/ConsumerForm";
+import ConsumerForm from "../../components/forms/ConsumerForm";
+import Footer from "../../components/ui/Footer";
 
 export default function SubmitFormPage({ form }) {
+  const [submitted, setSubmitted] = useState(false);
+
   return (
     <>
       <main className="flex flex-col items-center justify-center min-h-screen">
-        <ConsumerForm formData={form} />
+        {submitted ? (
+          <figure className="flex flex-col items-center justify-center gap-2 w-64 h-64 p-2 border rounded shadow-md bg-white">
+            <div className="flex items-center justify-center w-12 h-12 p-2 rounded-full bg-green-500">
+              <i className="bi bi-check-circle text-2xl text-white" />
+            </div>
+            <p className="text-center font-bold">Your answer has been submitted.</p>
+          </figure>
+        ) : (
+          <ConsumerForm formData={form} setSubmitted={setSubmitted} />
+        )}
       </main>
+      <Footer/>
     </>
   );
 }
