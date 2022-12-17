8641910c138d42200447a612007bd3dc8cb05fcc
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:13:12 -0400

    Added new middlewares

--- pages/api/account/email/index.js
+++ pages/api/account/email/index.js
@@ -1,3 +1,16 @@
-export default async function handler(req, res){
-    
+import {verifyToken} from "../../../../middlewares/authentication/jwt";
+import { changeAccountEmail } from "../../../../middlewares/account/email";
+
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "PUT":
+      return await verifyToken(req, res, changeAccountEmail);
+
+      break;
+
+    default:
+      return await res.status(405).json({ message: "" });
+      break;
+  }
 }
