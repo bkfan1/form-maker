ccd14e5b1ab8d43c62b99abe556214c7ffe0b8cc
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:12:46 -0400

    Added Link component

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,19 @@
 import LoginForm from "../../components/forms/LoginForm";
 import { verifyTokenServerSide } from "../../middlewares/authentication/jwt";
+import AppEmblem from "../../components/AppEmblem";
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center min-h-screen">
-        <LoginForm />
+      <main className="flex flex-col items-center justify-center gap-4 min-h-screen">
+        <AppEmblem />
+        <div className="flex flex-col gap-2">
+          <LoginForm />
+          <Link href="/register">
+            <span className="text-sm text-blue-500">Create account</span>
+          </Link>
+        </div>
       </main>
     </>
   );
