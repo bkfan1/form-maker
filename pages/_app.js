0c5e46f14f276bcc9aa29ead476329d2dbd81a55
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:14:05 -0400

    Wrapped Component with new contexts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
+import { AccountNameProvider } from "../contexts/AccountNameContext";
 import { ModalWindowProvider } from "../contexts/ModalWindowContext";
+import { SearchProvider } from "../contexts/SearchContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ModalWindowProvider>
-        <Component {...pageProps} />
+        <AccountNameProvider>
+          <SearchProvider>
+            <Component {...pageProps} />
+          </SearchProvider>
+        </AccountNameProvider>
       </ModalWindowProvider>
     </>
   );
