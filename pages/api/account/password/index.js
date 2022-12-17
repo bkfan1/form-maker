053c274cb2034e43dfa27c58f1eda2929636c58c
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:13:02 -0400

    Added new middlewares

--- pages/api/account/password/index.js
+++ pages/api/account/password/index.js
@@ -1,3 +1,16 @@
+import { verifyToken } from "../../../../middlewares/authentication/jwt";
+import { changeAccountPassword } from "../../../../middlewares/account/password";
+
 export default async function handler(req, res){
+    switch (req.method) {
+        case "PUT":
+            return await verifyToken(req, res, changeAccountPassword);
+            
+            break;
+    
+        default:
+            return await res.status(405).json({message: ""})
+            break;
+    }
     
 }
