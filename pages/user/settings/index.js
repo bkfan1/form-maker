8d29742457a93fbb72633be1927517be7898d627
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:09:54 -0400

    Added ChangeAccountEmailForm and ChangeAccountPassword components

--- pages/user/settings/index.js
+++ pages/user/settings/index.js
@@ -1,22 +1,63 @@
+import { verifyTokenServerSide } from "../../../middlewares/authentication/jwt";
+import { useState } from "react";
 
+import Layout from "../../../components/ui/Layout";
 
-export default function UserSettingsPage(){
-    return(
-        <>
-        </>
-    )
+import ChangeAccountEmailForm from "../../../components/forms/ChangeAccountEmailForm";
+import ChangeAccountPasswordForm from "../../../components/forms/ChangeAccountPasswordForm";
+
+import { nanoid } from "nanoid";
+
+const btns = [
+  {
+    id: nanoid(),
+    name: "email",
+    text: "Email",
+  },
+  {
+    id: nanoid(),
+    name: "password",
+    text: "Password",
+  },
+];
+
+export default function UserSettingsPage() {
+  const [section, setSection] = useState("email");
+  return (
+    <>
+      <Layout>
+        <main className="flex flex-col items-center justify-center min-h-screen">
+          <section className="flex flex-col items-center justify-center gap-4 w-full">
+            <header>
+              <menu className="flex gap-4 w-fit p-2 border rounded bg-white">
+                {btns.map((btn) => (
+                  <button
+                    onClick={() => setSection(btn.name)}
+                    key={btn.id}
+                    className={`border-b-2 ${
+                      section === btn.name && " border-b-indigo-800"
+                    }`}
+                  >
+                    {btn.text}
+                  </button>
+                ))}
+              </menu>
+            </header>
+
+            {section === "email" && <ChangeAccountEmailForm />}
+            {section === "password" && <ChangeAccountPasswordForm />}
+          </section>
+        </main>
+      </Layout>
+    </>
+  );
 }
 
 export async function getServerSideProps(ctx) {
-  const { cookie } = ctx.req.headers;
+  const token = await verifyTokenServerSide(ctx);
 
-  if (!cookie) {
-    return {
-      redirect: {
-        destination: "/login",
-        permanent: false,
-      },
-    };
+  if (!token) {
+    return { redirect: { destination: "/login", permanent: false } };
   }
 
   return {
