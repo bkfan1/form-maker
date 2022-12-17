b7fac6f9b31f91d6a192c13283ef7fd0edc4b47d
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:16:46 -0400

    Added new props

--- components/SubMenuItem.js
+++ components/SubMenuItem.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 
-export default function SubMenuItem({ icon, title }) {
+export default function SubMenuItem({ icon, title, action }) {
   return (
     <>
       <li
+        onClick={() => action()}
         className="flex gap-2 p-1 hover:bg-gray-200 hover:cursor-pointer"
       >
         <i className={icon} />
