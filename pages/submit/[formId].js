e3e3de7fca94a917f9ab69ed97a456c94278362f
bkfan1 <jpf8296@gmail.com>
2022-12-16 20:12:20 -0400

    Added new logic to handle form submits

--- pages/submit/[formId].js
+++ pages/submit/[formId].js
@@ -1,7 +1,5 @@
+import { getUniqueForm } from "../../middlewares/forms";
 import { useState } from "react";
-import connection from "../../database/connection";
-import Form from "../../database/models/form";
-
 import ConsumerForm from "../../components/forms/ConsumerForm";
 import Footer from "../../components/ui/Footer";
 
@@ -10,27 +8,32 @@ export default function SubmitFormPage({ form }) {
 
   return (
     <>
-      <main className="flex flex-col items-center justify-center min-h-screen">
+      <main className="flex flex-col items-center justify-center min-h-screen py-6">
         {submitted ? (
           <figure className="flex flex-col items-center justify-center gap-2 w-64 h-64 p-2 border rounded shadow-md bg-white">
             <div className="flex items-center justify-center w-12 h-12 p-2 rounded-full bg-green-500">
               <i className="bi bi-check-circle text-2xl text-white" />
             </div>
-            <p className="text-center font-bold">Your answer has been submitted.</p>
+            <p className="text-center font-bold">
+              Your answer has been submitted.
+            </p>
           </figure>
         ) : (
           <ConsumerForm formData={form} setSubmitted={setSubmitted} />
         )}
       </main>
-      <Footer/>
+      <Footer />
     </>
   );
 }
 
 export async function getServerSideProps(ctx) {
-  const db = await connection();
+  const found = await getUniqueForm(ctx.params.formId);
 
-  const found = await Form.findOne({ _id: ctx.params.formId }).lean();
+  if (!found) {
+    return { redirect: { destination: "/404", permanent: false } };
+  }
+
   const { _id, title, description, questions } = found;
   const form = { id: `${_id}`, title, description, questions };
 
