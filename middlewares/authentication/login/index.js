4cd28da6f21b9d8cd91920d78320429705ae427f
bkfan1 <jpf8296@gmail.com>
2022-12-21 15:10:51 -0400

    Improved middlewares

--- middlewares/authentication/login/index.js
+++ middlewares/authentication/login/index.js
@@ -9,14 +9,14 @@ export const handleLogin = async (req, res) => {
   const { body } = req;
 
   if (!body.email || !body.password) {
-    return await res.status(400).json({ message: "" });
+    return await res.status(400).json({ message: "Empty credentials." });
   }
 
   if (!email.test(body.email)) {
-    return await res.status(400).json({ message: "" });
+    return await res.status(400).json({ message: "Invalid email." });
   }
   if (body.password.length < 8) {
-    return await res.status(400).json({ message: "" });
+    return await res.status(400).json({ message: "Invalid password." });
   }
 
   const db = await connection();
@@ -24,14 +24,14 @@ export const handleLogin = async (req, res) => {
   const found = await Account.findOne({ email: body.email });
 
   if (!found) {
-    return await res.status(404).json({ message: "" });
+    return await res.status(404).json({ message: "Account doesn't exists." });
   }
 
   try {
     let equal = await compare(body.password, found.password);
 
     if (!equal) {
-      return await res.status(400).json({ message: "" });
+      return await res.status(400).json({ message: "Passwords dont match." });
     }
 
     const claims = { sub: "logged", accountId: found._id };
@@ -48,8 +48,8 @@ export const handleLogin = async (req, res) => {
 
     res.setHeader("Set-Cookie", cookie);
 
-    return await res.status(200).json({ accountName: found.fullname });
+    return await res.status(200).json({ accountName: found.fullname, message:"Logged sucessfully." });
   } catch (error) {
-    return await res.status(500).json({ message: "" });
+    return await res.status(500).json({ message: "Server error." });
   }
 };
