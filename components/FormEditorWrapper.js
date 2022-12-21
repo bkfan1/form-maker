97146e32c519ccb234c5480ec1371062e170e870
bkfan1 <jpf8296@gmail.com>
2022-12-21 15:17:50 -0400

    Added conditional rendering to display a preview of the form or the form editor

--- components/FormEditorWrapper.js
+++ components/FormEditorWrapper.js
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import { FormEditorContext } from "../contexts/FormEditorContext";
 import FormEditor from "./FormEditor";
 import FormEditorActionsMenu from "./FormEditorActionsMenu";
+import EndUserForm from "./forms/EndUserForm";
 
 export default function FormEditorWrapper() {
 
+  const {formData, previewForm} = useContext(FormEditorContext);
+
   return (
     <div className="flex gap-4 items-center">
-      <FormEditor />
+      {previewForm ? <EndUserForm formData={formData} /> : <FormEditor />}
       <FormEditorActionsMenu />
     </div>
   );
