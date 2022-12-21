e21a978c8292a783c4971a3c998edfb9d3d91bc5
bkfan1 <jpf8296@gmail.com>
2022-12-21 15:18:41 -0400

    Improved middlewares

--- middlewares/account/email/index.js
+++ middlewares/account/email/index.js
@@ -3,25 +3,42 @@ import Account from "../../../database/models/account";
 import { email } from "../../../utils/regex";
 
 export const changeAccountEmail = async (req, res, token) => {
+  if (!token) {
+    return await res.status(403).json({ message: "" });
+  }
+  const { accountId } = token;
   try {
-    const { accountId } = token;
-
     const { body } = req;
 
     if (!body.newEmail || !body.confirmNewEmail) {
-      return await res.status(400).json({ message: "" });
+      return await res.status(400).json({ message: "Empty credentials." });
     }
 
     if (body.newEmail !== body.confirmNewEmail) {
-      return await res.status(400).json({ message: "" });
+      return await res
+        .status(400)
+        .json({
+          message: "New email value doesn't match with confirm email value.",
+        });
     }
 
     if (body.confirmNewEmail !== body.newEmail) {
-      return await res.status(400).json({ message: "" });
+      return await res
+        .status(400)
+        .json({
+          message:
+            "Confirm new email value doesn't match with new email value.",
+        });
     }
 
-    if (!email.test(body.newEmail) || !email.test(body.confirmNewEmail)) {
-      return await res.status(400).json({ message: "" });
+    if (!email.test(body.newEmail)) {
+      return await res.status(400).json({ message: "Invalid new email." });
+    }
+
+    if (!email.test(body.confirmNewEmail)) {
+      return await res
+        .status(400)
+        .json({ message: "Invalid confirm new email." });
     }
 
     const db = await connection();
@@ -30,8 +47,10 @@ export const changeAccountEmail = async (req, res, token) => {
     const update = { email: body.newEmail };
 
     const found = await Account.findOneAndUpdate(filter, update);
-    return await res.status(200).json({ message: "" });
+    return await res
+      .status(200)
+      .json({ message: " Email updated succesfully" });
   } catch (error) {
-    return await res.status(500).json({ message: "" });
+    return await res.status(500).json({ message: "Server error. Try again." });
   }
 };
