7614e572effba11e507e1ec70551c62f2abef1ba
bkfan1 <jpf8296@gmail.com>
2022-12-21 15:24:04 -0400

    Added conditional rendering to display a textarea or paragraph tag depending on the type of question input

--- components/SubmissionDetailsContainer.js
+++ components/SubmissionDetailsContainer.js
@@ -1,21 +1,38 @@
 export default function SubmissionDetailsContainer({ data }) {
-  const { id, answers, submittedAt } = data;
+  const { answers, submittedAt } = data;
 
   return (
     <>
-      <figure className="flex flex-col gap-2 min-w-[368px] p-2 border rounded bg-white">
+      <figure className="flex flex-col gap-2 w-[400px] p-2 border rounded bg-white">
         <header>
           <h1 className="text-2xl font-bold">Submission</h1>
           <h2 className="text-sm text-gray-500">
-            <i className="bi bi-calendar"/>
+            <i className="bi bi-calendar" />
             <span> {new Date(submittedAt).toLocaleString()}</span>
           </h2>
         </header>
-        <section className="flex flex-col">
+        <section className="hiddeScrollbar flex flex-col gap-2 h-96 overflow-y-scroll">
           {answers.map(({ id, question, answer }) => (
-            <div key={id} className="answer__body gap-2 p-2 rounded odd:bg-gray-200">
+            <div
+              key={id}
+              className="answer__body gap-2 p-2 rounded odd:bg-gray-200"
+            >
               <h1 className="text-xl font-bold">{question.title}</h1>
-              <p className="text-gray-600">{answer}</p>
+              <details>
+                <summary>Details</summary>
+                {question.option === "short-answer" ||
+                question.option === "date" ||
+                question.option === "linear-range" ? (
+                  <p>{answer}</p>
+                ) : (
+                  ""
+                )}
+                {question.option === "paragraph" && (
+                  <textarea className="w-full h-52 resize-none" disabled>
+                    {answer}
+                  </textarea>
+                )}
+              </details>
             </div>
           ))}
         </section>
