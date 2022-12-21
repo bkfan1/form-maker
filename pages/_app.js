56a00f2524c4f6e83225167200208737e18bbfaa
bkfan1 <jpf8296@gmail.com>
2022-12-21 15:11:44 -0400

    Imported Toaster component from react-hot-toast lib

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { AccountNameProvider } from "../contexts/AccountNameContext";
 import { ModalWindowProvider } from "../contexts/ModalWindowContext";
 import { SearchProvider } from "../contexts/SearchContext";
 import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }) {
       <ModalWindowProvider>
         <AccountNameProvider>
           <SearchProvider>
+            <Toaster />
             <Component {...pageProps} />
           </SearchProvider>
         </AccountNameProvider>
